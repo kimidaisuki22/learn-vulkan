36b6da1d7bcfe9e218f08665f81be32f43911a8b
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2022-10-23 19:02:20 +0800

    complete simple vulkan program.

--- draw-triangle.cpp
+++ draw-triangle.cpp
@@ -1,6 +1,29 @@
 #include "tiny-vulkan.h"
 #include <iostream>
+#include <format>
 
 int main(){
-    std::cout << "Hello Vulkan.\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    int width = 1920 ;
+    int height = width * 16 / 9;
+    GLFWwindow* window = glfwCreateWindow(width,height,"Vulkan Window",nullptr,nullptr);
+
+    uint32_t extensionCount {};
+    vkEnumerateInstanceExtensionProperties(nullptr,&extensionCount,nullptr);
+
+    std::cout << std::format ("{} extension supported.\n");
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+
+    auto test = matrix * vec;
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
